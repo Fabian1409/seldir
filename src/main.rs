aa5c55cb2aa146e8fb0eac54805b27094a86b28d
Fabian Gruber <fabian.gruber@student.tugraz.at>
2023-02-24 22:31:37 +0100

    cleanup, write cd target to tmp file

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,59 @@
 use cursive::event::EventResult;
 use cursive::theme::{Theme, BorderStyle};
-use cursive::{traits::*, Cursive};
-use cursive::view::Selector;
-use cursive::views::{Dialog, OnEventView, SelectView, TextView, LinearLayout, Layer, TextContent, NamedView, ViewRef};
-use std::fs;
+use cursive::view::{Nameable, Resizable};
+use cursive::views::{Dialog, SelectView, LinearLayout, Layer};
+use std::io::Write;
+use std::{fs, fs::File};
+use std::env;
+
+struct Item {
+    path: String,
+    is_dir: bool,
+}
+
+impl Item {
+    fn new(path: String) -> Item {
+        let metadata = fs::metadata(path.clone()).unwrap();
+        let is_dir = metadata.is_dir();
+        Item { path, is_dir }
+    }
+}
+
+fn populate_select(select: &mut SelectView<Item>, path: String) {
+    let dirs = fs::read_dir(path.clone()).unwrap();
+    for dir in dirs {
+        let dir_str = dir.unwrap().path().to_str().unwrap().to_owned();
+        let item = Item::new(dir_str.clone());
+        select.add_item(dir_str.replace(&path, ""), item);
+    }
+}
 
 fn main() {
-
-    let mut layer = Layer::new(LinearLayout::horizontal().with_name("splits"));
-    let mut select_parent = SelectView::<String>::new().with_name("parent");
-    let mut select_current = SelectView::<String>::new().with_name("current");
-
-    select_current.get_mut().set_on_submit(|s: &mut Cursive, item: &String| {
-        let mut view: ViewRef<LinearLayout> = s.find_name("splits").unwrap();
-        view.swap_children(0, 1);
-    });
-    
-    let paths_current = fs::read_dir("./").unwrap();
-    let paths_parent = fs::read_dir("../").unwrap();
-
-    for path in paths_current {
-        select_current.get_mut().add_item_str(path.unwrap().path().to_str().unwrap().replace("./", ""));
-    }
-
-    for path in paths_parent {
-        select_parent.get_mut().add_item_str(path.unwrap().path().to_str().unwrap().replace("../", ""));
-    }
-
-    let select_current = OnEventView::new(select_current)
-        .on_pre_event_inner('k', |s, _| {
-            let cb = s.get_mut().select_up(1);
-            Some(EventResult::Consumed(Some(cb)))
-        })
-        .on_pre_event_inner('j', |s, _| {
-            let cb = s.get_mut().select_down(1);
-            Some(EventResult::Consumed(Some(cb)))
-        });
-
     let mut siv = cursive::default();
     let mut theme = Theme::terminal_default();
     theme.borders = BorderStyle::None;
-
     siv.set_theme(theme);
 
+    let mut select_prev = SelectView::<Item>::new().with_name("prev");
+    let mut select_next = SelectView::<Item>::new().with_name("next");
+    let mut select_curr = SelectView::<Item>::new().with_name("curr");
 
-    layer.get_inner_mut().get_mut().add_child(Dialog::around(select_parent).title("parent").full_height().full_width());
-    layer.get_inner_mut().get_mut().add_child(Dialog::around(select_current).title("current").full_height().full_width());
-    layer.get_inner_mut().get_mut().add_child(Dialog::around(TextView::new_with_content(TextContent::new("content"))).title("preview").full_height().full_width());
+    populate_select(&mut select_prev.get_mut(), String::from("../"));
+    populate_select(&mut select_curr.get_mut(), String::from("./"));
+    populate_select(&mut select_next.get_mut(), String::from("./"));
+
+    let mut layer = Layer::new(LinearLayout::horizontal());
+    layer.get_inner_mut().add_child(Dialog::around(select_prev).title("prev").full_height().full_width());
+    layer.get_inner_mut().add_child(Dialog::around(select_curr).title("curr").full_height().full_width());
+    layer.get_inner_mut().add_child(Dialog::around(select_next).title("next").full_height().full_width());
 
     siv.add_layer(layer);
-    siv.focus_name("current").expect("current not found");
-
-
+    siv.focus_name("curr").expect("curr not found");
+    siv.add_global_callback('q', |s| {
+        let cwd = env::current_dir().unwrap().to_str().expect("failed to get cwd").to_owned();
+        let mut file = File::create(format!("/tmp/seldir")).expect("failed to create tmp file");
+        file.write_all(cwd.as_bytes()).expect("failed writing to file");
+        s.quit();
+    });
     siv.run();
-}
-
-fn on_select(layer: &mut Layer<LinearLayout>) {
-    layer.get_inner_mut().swap_children(0, 1);
 }
