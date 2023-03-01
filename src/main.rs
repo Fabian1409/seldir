6329c29c6e40b184ca6badb2807ab7aacea9fb64
Fabian Gruber <fabian.gruber5@gmail.com>
2023-03-01 21:47:14 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use cursive::Cursive;
 use cursive::theme::{Theme, BorderStyle, ColorStyle, Color};
 use cursive::utils::markup::StyledString;
-use cursive::view::{Nameable, Resizable, Scrollable};
-use cursive::views::{Dialog, SelectView, LinearLayout, Layer, ViewRef};
+use cursive::view::{Nameable, Resizable, Scrollable, Margins};
+use cursive::views::{Dialog, SelectView, LinearLayout, Layer, ViewRef, TextView};
 use std::cmp::Ordering;
 use std::str::FromStr;
 use std::fs;
@@ -26,12 +26,15 @@ fn update_selects(s: &mut Cursive, is_enter: bool) {
     let mut prev_dialog: ViewRef<Dialog> = s.find_name("prev_dialog").unwrap();
     let curr_select = curr_dialog.get_content_mut().downcast_mut::<SelectView<Item>>().unwrap();
     let prev_select = prev_dialog.get_content_mut().downcast_mut::<SelectView<Item>>().unwrap();
-    let selection = curr_select.selection().unwrap().path.clone();
+    let selection = curr_select.selection().unwrap();
 
-    if is_enter {
-        env::set_current_dir(&selection).expect("failed to set dir");
-    } else {
-        env::set_current_dir("../").expect("failed to set dir");
+    if !selection.is_dir && is_enter {
+        return;
+    }
+
+    if (is_enter && env::set_current_dir(&selection.path).is_err()) ||
+        env::set_current_dir("../").is_err() {
+        return;
     }
 
     curr_select.clear();
@@ -41,14 +44,14 @@ fn update_selects(s: &mut Cursive, is_enter: bool) {
     update_next(s, &curr_select.selection().unwrap());
 
     if is_enter {
-        let id = prev_select.iter().position(|item| item.1.path.contains(&selection)).unwrap();
+        let id = prev_select.iter().position(|item| item.1.path.contains(&selection.path)).unwrap();
         prev_select.set_selection(id);
     } else {
-        let cwd = env::current_dir().unwrap().to_str().unwrap().to_owned();
-        let parent_dir = cwd.split('/').last().unwrap();
-        let id = prev_select.iter().position(|item| item.1.path.contains(parent_dir)).unwrap();
-        prev_select.set_selection(id);
+        update_prev_selection(prev_select);
     };
+
+    let mut path_text: ViewRef<TextView> = s.find_name("path_text").unwrap();
+    path_text.set_content(env::current_dir().unwrap().to_str().unwrap());
 }
 
 fn update_next(s: &mut Cursive, item: &Item) {
@@ -60,11 +63,26 @@ fn update_next(s: &mut Cursive, item: &Item) {
     }
 }
 
-fn populate_select(select: &mut SelectView<Item>, path: String) {
-    let read_dir = fs::read_dir(path.clone()).unwrap();
-    let mut dirs: Vec<String> = read_dir.map(|x| x.unwrap().path().to_str().unwrap().to_owned())
-        .filter(|x| !fs::symlink_metadata(x).unwrap().is_symlink())
+fn update_prev_selection(prev_select: &mut SelectView<Item>) {
+    let cwd = env::current_dir().unwrap().to_str().unwrap().to_owned();
+    let parent_dir = cwd.split('/').last().unwrap();
+    let id = prev_select.iter().position(|item| item.1.path.contains(parent_dir)).unwrap();
+    prev_select.set_selection(id);
+}
+
+fn read_dir_custom(path: &str) -> Option<Vec<String>> {
+    let read_dir = fs::read_dir(path);
+    if read_dir.is_err() {
+        return None;
+    }
+    let mut dirs: Vec<String> = fs::read_dir(path).unwrap()
+        .filter_map(|x| x.ok())
+        .map(|x| x.path().to_string_lossy().into_owned())
+        .filter(|x| 
+            !fs::symlink_metadata(x).unwrap().is_symlink() &&
+            !x.split('/').last().unwrap().starts_with('.'))
         .collect();
+
     dirs.sort_by(|a, b| {
         let a_meta = fs::metadata(a).unwrap();
         let b_meta = fs::metadata(b).unwrap();
@@ -78,16 +96,26 @@ fn populate_select(select: &mut SelectView<Item>, path: String) {
             a.cmp(b)
         }
     });
+    Some(dirs)
+}
 
-    for dir in dirs {
-        let label_str = dir.replace(&path, "").replace('/', "");
-        let item = Item::new(dir);
-        let mut style = ColorStyle::terminal_default();
-        if item.is_dir {
-            style.front = cursive::theme::ColorType::Color(Color::from_str("red").unwrap());
-        } 
-        let label = StyledString::styled(label_str, style);
-        select.add_item(label, item);
+fn populate_select(select: &mut SelectView<Item>, path: String) {
+    match read_dir_custom(&path) {
+        Some(dirs) => {
+                for dir in dirs {
+                let label_str = dir.replace(&path, "").replace('/', "");
+                let item = Item::new(dir);
+                let mut style = ColorStyle::terminal_default();
+                if item.is_dir {
+                    style.front = cursive::theme::ColorType::Color(Color::from_str("red").unwrap());
+                } 
+                select.add_item(StyledString::styled(label_str, style), item);
+            }
+        },
+        None => {
+            let dummy_item = Item { path: "".to_owned(), is_dir: false };
+            select.add_item("...", dummy_item);
+        }
     }
 }
 
@@ -101,17 +129,12 @@ fn main() {
 
     let mut prev_select = SelectView::<Item>::new().disabled();
     let mut curr_select = SelectView::<Item>::new();
-    let mut next_select = SelectView::<Item>::new().disabled();
-
-    next_select.set_inactive_highlight(false);
+    let mut next_select = SelectView::<Item>::new().disabled().with_inactive_highlight(false);
 
     populate_select(&mut prev_select, String::from("../"));
     populate_select(&mut curr_select, String::from("./"));
 
-    let cwd = env::current_dir().unwrap().to_str().unwrap().to_owned();
-    let parent_dir = cwd.split('/').last().unwrap();
-    let prev_selection_id = prev_select.iter().position(|item| item.1.path.contains(parent_dir)).unwrap();
-    prev_select.set_selection(prev_selection_id);
+    update_prev_selection(&mut prev_select);
 
     let curr_selection = curr_select.selection().unwrap();
     if curr_selection.is_dir {
@@ -122,29 +145,41 @@ fn main() {
 
     siv.add_layer(
         Layer::new(
-            LinearLayout::horizontal()
+            LinearLayout::vertical()
                 .child(
-                    Dialog::new()
-                        .content(prev_select)
-                        .with_name("prev_dialog")
-                        .full_height()
-                        .fixed_width(20)
+                    TextView::new(
+                        env::current_dir().unwrap()
+                            .to_string_lossy()
+                    ).with_name("path_text")
                 )
                 .child(
-                    Dialog::new()
-                        .content(curr_select)
-                        .with_name("curr_dialog")
-                        .scrollable()
-                        .show_scrollbars(false)
-                        .full_height()
-                        .full_width()
-                )
-                .child(
-                    Dialog::new()
-                        .content(next_select)
-                        .with_name("next_dialog")
-                        .full_height()
-                        .full_width()
+                LinearLayout::horizontal()
+                    .child(
+                        Dialog::new()
+                            .padding(Margins::zeroes())
+                            .content(prev_select)
+                            .with_name("prev_dialog")
+                            .full_height()
+                            .fixed_width(20)
+                    )
+                    .child(
+                        Dialog::new()
+                            .padding(Margins::zeroes())
+                            .content(curr_select)
+                            .with_name("curr_dialog")
+                            .scrollable()
+                            .show_scrollbars(false)
+                            .full_height()
+                            .full_width()
+                    )
+                    .child(
+                        Dialog::new()
+                            .padding(Margins::zeroes())
+                            .content(next_select)
+                            .with_name("next_dialog")
+                            .full_height()
+                            .full_width()
+                    )
                 )
         ),
     );
