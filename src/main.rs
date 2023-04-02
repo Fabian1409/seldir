d6ead2a509cc294fc16bd3d3da7c90327a6ea321
Fabian Gruber <fabian.gruber5@gmail.com>
2023-04-02 17:19:52 +0200

    removed file preview, added custom accent color as cmd line arg

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+use clap::{arg, command};
 use cursive::event::{Event, Key};
-use cursive::theme::{BaseColor, Color, ColorStyle, ColorType, PaletteColor, Theme};
+use cursive::theme::{Color, ColorStyle, ColorType, PaletteColor, Theme};
 use cursive::utils::markup::StyledString;
 use cursive::view::{Nameable, Resizable, Scrollable};
 use cursive::views::{
     EditView, Layer, LinearLayout, ScrollView, SelectView, ShadowView, TextView, ViewRef,
 };
 use cursive::Cursive;
-use cursive_async_view::AsyncView;
-use cursive_extras::{hlayout, vlayout, ImageView};
-use cursive_hexview::HexView;
-use pdf_extract::extract_text;
+use cursive_extras::{hlayout, vlayout};
 use std::cmp::Ordering;
 use std::fs::DirEntry;
 use std::path::Path;
 use std::{env, fs};
+
+const CURR_NAME: &str = "curr";
+const PREV_NAME: &str = "prev";
+const NEXT_NAME: &str = "next";
+const SEARCH_NAME: &str = "search";
+const HLAYOUT_NAME: &str = "hlayout";
+const PATH_TEXT_NAME: &str = "path_text";
 
 #[derive(Default)]
 struct State {
@@ -22,9 +27,9 @@ struct State {
 }
 
 fn update_prev_curr(s: &mut Cursive, is_enter: bool) {
-    let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+    let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
     let curr_select = curr.get_inner_mut();
-    let mut prev: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("prev").unwrap();
+    let mut prev: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(PREV_NAME).unwrap();
     let prev_select = prev.get_inner_mut();
 
     let prev_selection = prev_select.selected_id();
@@ -45,17 +50,15 @@ fn update_prev_curr(s: &mut Cursive, is_enter: bool) {
         return;
     }
 
-    let show_hidden = s.user_data::<State>().unwrap().show_hidden;
-
     if env::current_dir().unwrap().ancestors().count() <= 1 {
         prev_select.clear();
     } else {
         populate_select(
+            s,
             prev_select,
             env::current_dir().unwrap().parent().unwrap(),
-            show_hidden,
         );
-        update_prev(prev_select);
+        update_prev_selection(prev_select);
         prev.scroll_to_important_area();
     }
 
@@ -64,7 +67,7 @@ fn update_prev_curr(s: &mut Cursive, is_enter: bool) {
         return;
     }
 
-    populate_select(curr_select, &env::current_dir().unwrap(), show_hidden);
+    populate_select(s, curr_select, &env::current_dir().unwrap());
     if !is_enter {
         curr_select.set_selection(prev_selection.unwrap());
     }
@@ -72,15 +75,16 @@ fn update_prev_curr(s: &mut Cursive, is_enter: bool) {
     match curr_select.selection() {
         Some(item) => update_next(s, &item),
         None => {
-            let mut next: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("next").unwrap();
+            let mut next: ViewRef<ScrollView<SelectView<DirEntry>>> =
+                s.find_name(NEXT_NAME).unwrap();
             next.get_inner_mut().clear();
         }
     }
 }
 
 fn update_next(s: &mut Cursive, item: &DirEntry) {
-    let mut hlayout: ViewRef<LinearLayout> = s.find_name("hlayout").unwrap();
-    let mut path_text: ViewRef<TextView> = s.find_name("path_text").unwrap();
+    let mut hlayout: ViewRef<LinearLayout> = s.find_name(HLAYOUT_NAME).unwrap();
+    let mut path_text: ViewRef<TextView> = s.find_name(PATH_TEXT_NAME).unwrap();
     path_text.set_content(format!(
         "{}/{}",
         env::current_dir().unwrap().to_string_lossy(),
@@ -92,61 +96,16 @@ fn update_next(s: &mut Cursive, item: &DirEntry) {
             .disabled()
             .with_inactive_highlight(false);
 
-        let show_hidden = s.user_data::<State>().unwrap().show_hidden;
-        populate_select(&mut next_select, &item.path(), show_hidden);
+        populate_select(s, &mut next_select, &item.path());
         hlayout.add_child(
             ShadowView::new(next_select)
                 .top_padding(false)
                 .min_width(30),
         );
-    } else {
-        let name = item.file_name();
-        let file_ext = name.to_str().unwrap().split('.').last().unwrap();
-        match file_ext {
-            "pdf" => {
-                let path = item.path();
-                hlayout.add_child(
-                    ShadowView::new(
-                        AsyncView::new_with_bg_creator(
-                            s,
-                            move || {
-                                let data = extract_text(path).unwrap_or("pdf".to_owned());
-                                Ok(data)
-                            },
-                            TextView::new,
-                        )
-                        .min_width(50),
-                    )
-                    .top_padding(false),
-                );
-            }
-            "png" | "jpg" | "jpeg" => {
-                hlayout.add_child(
-                    ShadowView::new(
-                        ImageView::new(50, 18)
-                            .image(&item.path().to_string_lossy())
-                            .min_width(50),
-                    )
-                    .top_padding(false),
-                );
-            }
-            _ => match fs::read_to_string(item.path()) {
-                Ok(mut text) => {
-                    text.truncate(1_000);
-                    hlayout.add_child(ShadowView::new(TextView::new(text)).top_padding(false))
-                }
-                Err(_) => {
-                    let mut data = fs::read(item.path()).unwrap();
-                    data.truncate(1_000);
-                    hlayout
-                        .add_child(ShadowView::new(HexView::new_from_iter(data)).top_padding(false))
-                }
-            },
-        };
     }
 }
 
-fn update_prev(prev_select: &mut SelectView<DirEntry>) {
+fn update_prev_selection(prev_select: &mut SelectView<DirEntry>) {
     let id = prev_select
         .iter()
         .position(|item| item.1.path().eq(&env::current_dir().unwrap()))
@@ -185,13 +144,14 @@ fn read_dir_sorted(path: &Path, show_hidden: bool) -> Vec<DirEntry> {
     }
 }
 
-fn populate_select(select: &mut SelectView<DirEntry>, path: &Path, show_hidden: bool) {
+fn populate_select(s: &mut Cursive, select: &mut SelectView<DirEntry>, path: &Path) {
+    let show_hidden = s.user_data::<State>().unwrap().show_hidden;
     select.clear();
     let entries = read_dir_sorted(path, show_hidden);
     for e in entries {
         let mut style = ColorStyle::terminal_default();
         if e.path().is_dir() {
-            style.front = ColorType::Color(Color::Dark(BaseColor::Red));
+            style.front = ColorType::Color(s.current_theme().palette[PaletteColor::Highlight]);
         }
         select.add_item(
             StyledString::styled(e.path().file_name().unwrap().to_string_lossy(), style),
@@ -201,7 +161,7 @@ fn populate_select(select: &mut SelectView<DirEntry>, path: &Path, show_hidden: 
 }
 
 fn submit_search(s: &mut Cursive, text: &str) {
-    let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+    let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
     let curr_select = curr.get_inner_mut();
     let query = text.replace("search: ", "").to_ascii_lowercase();
     let result = curr_select.iter().find(|x| {
@@ -211,53 +171,78 @@ fn submit_search(s: &mut Cursive, text: &str) {
         let item_id = curr_select.iter().position(|x| x.0.eq(item.0)).unwrap();
         let cb = curr_select.set_selection(item_id);
         cb(s);
-        let mut search: ViewRef<EditView> = s.find_name("search").unwrap();
+        let mut search: ViewRef<EditView> = s.find_name(SEARCH_NAME).unwrap();
         search.set_content("");
         search.disable();
     }
 }
 
 fn init(s: &mut Cursive) {
-    let show_hidden = s.user_data::<State>().unwrap().show_hidden;
-    let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+    let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
     let curr_select = curr.get_inner_mut();
-    populate_select(curr_select, &env::current_dir().unwrap(), show_hidden);
+    populate_select(s, curr_select, &env::current_dir().unwrap());
 
-    let mut prev: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("prev").unwrap();
+    let mut prev: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(PREV_NAME).unwrap();
     let prev_select = prev.get_inner_mut();
     populate_select(
+        s,
         prev_select,
         env::current_dir().unwrap().parent().unwrap(),
-        show_hidden,
     );
-    update_prev(prev_select);
+    update_prev_selection(prev_select);
     prev.scroll_to_important_area();
 
     let curr_selection = curr_select.selection().unwrap();
     update_next(s, &curr_selection);
 }
 
+fn handle_exit(s: &mut Cursive) {
+    let curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
+    let selection = curr.get_inner().selection();
+    let path = match selection {
+        Some(dir) => {
+            if dir.metadata().unwrap().is_dir() {
+                dir.path()
+            } else {
+                env::current_dir().unwrap()
+            }
+        }
+        None => env::current_dir().unwrap(),
+    }
+    .to_str()
+    .unwrap()
+    .to_owned();
+    fs::write("/tmp/seldir", path).unwrap();
+    s.quit();
+}
+
 fn main() {
     let mut siv = cursive::default();
-    // let mut siv = cursive_extras::buffered_backend_root();
-
     let mut theme = Theme::terminal_default();
-    theme.palette[PaletteColor::Highlight] = Color::Dark(BaseColor::Red);
-    theme.palette[PaletteColor::HighlightInactive] = Color::Dark(BaseColor::Red);
+    let matches = command!()
+        .arg(arg!(--accent_color [accent_color] "Accent color for seldir"))
+        .get_matches();
+    let accent_color = if let Some(color) = matches.get_one::<String>("accent_color") {
+        Color::parse(color).unwrap()
+    } else {
+        Color::parse("red").unwrap()
+    };
+    theme.palette[PaletteColor::Highlight] = accent_color;
+    theme.palette[PaletteColor::HighlightInactive] = accent_color;
     siv.set_theme(theme);
 
     let state = State::default();
     siv.set_user_data(state);
 
     siv.add_fullscreen_layer(Layer::new(vlayout!(
-        TextView::new(env::current_dir().unwrap().to_string_lossy()).with_name("path_text"),
+        TextView::new(env::current_dir().unwrap().to_string_lossy()).with_name(PATH_TEXT_NAME),
         hlayout!(
             ShadowView::new(
                 SelectView::<DirEntry>::new()
                     .disabled()
                     .scrollable()
                     .show_scrollbars(false)
-                    .with_name("prev")
+                    .with_name(PREV_NAME)
                     .fixed_width(15)
             )
             .top_padding(false)
@@ -267,63 +252,42 @@ fn main() {
                     .on_select(update_next)
                     .scrollable()
                     .show_scrollbars(false)
-                    .with_name("curr")
+                    .with_name(CURR_NAME)
                     .min_width(30)
             )
             .top_padding(false)
         )
-        .with_name("hlayout")
+        .with_name(HLAYOUT_NAME)
         .full_height(),
         EditView::new()
             .disabled()
             .filler(" ")
             .on_submit(submit_search)
-            .with_name("search")
+            .with_name(SEARCH_NAME)
             .fixed_height(1)
     )));
 
     init(&mut siv);
 
     siv.focus_name("curr").unwrap();
-    siv.add_global_callback('q', |s| {
-        let curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
-        let selection = curr.get_inner().selection();
-        let path = match selection {
-            Some(dir) => {
-                if dir.metadata().unwrap().is_dir() {
-                    dir.path()
-                } else {
-                    env::current_dir().unwrap()
-                }
-            }
-            None => env::current_dir().unwrap(),
-        }
-        .to_str()
-        .unwrap()
-        .to_owned();
-        fs::write("/tmp/seldir", path).unwrap();
-        s.quit();
-    });
+    siv.add_global_callback('q', handle_exit);
+    siv.add_global_callback(Key::Enter, handle_exit);
     siv.add_global_callback('j', |s| {
-        let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+        let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
         let cb = curr.get_inner_mut().select_down(1);
         cb(s);
         curr.scroll_to_important_area();
     });
     siv.add_global_callback('k', |s| {
-        let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+        let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
         let cb = curr.get_inner_mut().select_up(1);
         cb(s);
         curr.scroll_to_important_area();
     });
-    siv.add_global_callback('l', |s| {
-        update_prev_curr(s, true);
-    });
-    siv.add_global_callback('h', |s| {
-        update_prev_curr(s, false);
-    });
+    siv.add_global_callback('l', |s| update_prev_curr(s, true));
+    siv.add_global_callback('h', |s| update_prev_curr(s, false));
     siv.add_global_callback('G', |s| {
-        let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+        let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name(CURR_NAME).unwrap();
         let curr_select = curr.get_inner_mut();
         let cb = curr_select.set_selection(curr_select.len() - 1);
         cb(s);
@@ -332,7 +296,8 @@ fn main() {
     siv.add_global_callback('g', |s| {
         let pressed_g = s.user_data::<State>().unwrap().pressed_g;
         if pressed_g {
-            let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> = s.find_name("curr").unwrap();
+            let mut curr: ViewRef<ScrollView<SelectView<DirEntry>>> =
+                s.find_name(CURR_NAME).unwrap();
             let cb = curr.get_inner_mut().set_selection(0);
             cb(s);
             curr.scroll_to_important_area();
@@ -347,15 +312,15 @@ fn main() {
         init(s);
     });
     siv.add_global_callback('/', |s| {
-        let mut search: ViewRef<EditView> = s.find_name("search").unwrap();
+        let mut search: ViewRef<EditView> = s.find_name(SEARCH_NAME).unwrap();
         let text = "search: ";
         search.set_content(text);
         search.set_cursor(text.len());
         search.enable();
-        s.focus_name("search").unwrap();
+        s.focus_name(SEARCH_NAME).unwrap();
     });
     siv.add_global_callback(Event::Key(Key::Esc), |s| {
-        let mut search: ViewRef<EditView> = s.find_name("search").unwrap();
+        let mut search: ViewRef<EditView> = s.find_name(SEARCH_NAME).unwrap();
         if search.is_enabled() {
             search.set_content("");
             search.disable();
